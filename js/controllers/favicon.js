691f722a043fb62bcdd275a314e250546a9594a4
Antoinette Janus <acj.gfx@gmail.com>
2016-05-03 19:45:22 -0400

    Whoops scopes wrong

--- js/controllers/favicon.js
+++ js/controllers/favicon.js
@@ -1,7 +1,6 @@
 typicalAgency.controller('Favicon', function($scope, $http){
   $http.get('/api/colors')
-  .success(function(colors){
-    console.log(colors[randNum(0, colors.length)]);
-    $scope.faviconColor = colors[randNum(0, colors.length)].name;
+  .success(function(data){
+    $scope.faviconColor = data[0].colors[randNum(0, colors.length)].name;
   });
 });
