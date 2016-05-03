bafe0f3395dda2f349077771d98c0ef48cfcca21
Antoinette Janus <acj.gfx@gmail.com>
2016-05-03 19:56:45 -0400

    fixed the icon api scope

--- js/controllers/favicon.js
+++ js/controllers/favicon.js
@@ -2,6 +2,6 @@ typicalAgency.controller('Favicon', function($scope, $http){
   $http.get('/api/colors')
   .success(function(data){
     console.log(data[0].colors);
-    $scope.faviconColor = data[0].colors[randNum(0, colors.length)].name;
+    $scope.faviconColor = data[0].colors[randNum(0, data[0].colors.length)].name;
   });
 });
