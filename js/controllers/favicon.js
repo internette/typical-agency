1bb2c5b6141151f4fed3c1bc9d7ca350eb7791f5
Antoinette Janus <acj.gfx@gmail.com>
2016-05-03 19:53:10 -0400

    test

--- js/controllers/favicon.js
+++ js/controllers/favicon.js
@@ -1,6 +1,7 @@
 typicalAgency.controller('Favicon', function($scope, $http){
   $http.get('/api/colors')
   .success(function(data){
+    console.log(data[0].colors);
     $scope.faviconColor = data[0].colors[randNum(0, colors.length)].name;
   });
 });
