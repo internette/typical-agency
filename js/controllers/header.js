e7933b3ef2116abf65a1f8ea3a3787b5911b4369
Antoinette Janus <acj.gfx@gmail.com>
2016-05-03 20:11:11 -0400

    Test

--- js/controllers/header.js
+++ js/controllers/header.js
@@ -38,8 +38,10 @@ typicalAgency.controller('Header', function($scope, $http){
       var colors = data[0].colors;
       var color = colors[randNum(0, colors.length)];
       $scope.bgColor = color.value;
-      brandColor.value = color.value;
-      brandColor.name = color.name;
+      brandColor.push({
+        name: color.name,
+        value: color.value
+      });
     })
     .error(function(data, status, headers, config){});
     });
