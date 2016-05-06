3d2acc3264d1f80e8065f9bbd1f82ca6c0e0b128
Antoinette Janus <acj.gfx@gmail.com>
2016-05-06 14:32:19 -0400

    Please work

--- server/server.js
+++ server/server.js
@@ -45,7 +45,6 @@ router.use(function(req, res, next) {
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 app.use(express.static(path.join(__dirname + '/../')));
 router.route('/').get(function(req, res) {
-    console.log('hey there');
     res.sendFile('./index.html');
 
 });
