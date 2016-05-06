46fef8338fa9b4b9af155360f12f454bc47ec175
Antoinette Janus <acj.gfx@gmail.com>
2016-05-06 14:09:28 -0400

    ugh

--- server/server.js
+++ server/server.js
@@ -45,9 +45,7 @@ router.use(function(req, res, next) {
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 app.use(express.static(path.join(__dirname + '/../')));
 router.route('/').get(function(req, res) {
-    fs.readdir('./', function(d){
-      console.log(d);
-    });
+    console.log('hey there');
     res.sendFile('./index.html');
 
 });
