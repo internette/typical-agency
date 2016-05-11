55cfecdcfb9e7755c8b61100991a187fdc047764
Antoinette Janus <acj.gfx@gmail.com>
2016-05-11 17:23:01 -0400

    Delete color.js

--- api/models/color.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var ColorSchema   = new Schema({
-    name: String,
-    hex: String
-});
-
-module.exports = mongoose.model('Color', ColorSchema);
