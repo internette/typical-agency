0a0681cb7d705df8fd8355d7300f845167ca95c3
Antoinette Janus <acj.gfx@gmail.com>
2016-05-11 17:23:05 -0400

    Delete image.js

--- api/models/image.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var ImageSchema   = new Schema({
-    name: String,
-    url: String
-});
-
-module.exports = mongoose.model('Image', ImageSchema);
