fd638d18ac2209b03cd8ef1c40e0c8202a0d4e8e
Antoinette Janus <acj.gfx@gmail.com>
2016-05-11 17:24:55 -0400

    Updated

--- server/server.js
+++ server/server.js
@@ -9,12 +9,13 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
-var Word = require('../api/models/word');
-var Color = require('../api/models/color');
-var Images = require('../api/models/image');
-var Bio = require('../api/models/bio');
-var Client = require('../api/models/client');
-var Person = require('../api/models/person');
+var Data = require('../api/models/data');
+// var Word = require('../api/models/word');
+// var Color = require('../api/models/color');
+// var Images = require('../api/models/image');
+// var Bio = require('../api/models/bio');
+// var Client = require('../api/models/client');
+// var Person = require('../api/models/person');
 
 mongoose.connect('mongodb://twaffles:sakura@ec2-52-73-225-190.compute-1.amazonaws.com:27017/dummyDB'); // connect to our database
 //
@@ -57,70 +58,82 @@ router.route('/api').get(function(req, res) {
 // more routes for our API will happen here
 // on routes that end in /words
 // ----------------------------------------------------
-router.route('/api/words')
-    // retrieve words
-    .get(function(req, res) {
-        Word.find(function(err, words) {
-            if (err)
-                res.send(err);
-
-            res.jsonp(words);
-        });
-    });
-    router.route('/api/words/buzzwords')
-        // retrieve words
-        .get(function(req, res) {
-            Word.find(function(err, words) {
-                if (err)
-                    res.send(err);
-
-                res.jsonp(words[0].toJSON().buzzwords);
-            });
-        });
-router.route('/api/colors')
-  .get(function(req,res){
-    Color.find(function(err, colors) {
-      if (err)
-        res.send(err);
-
-      res.jsonp(colors);
-    });
-  });
-  router.route('/api/images')
-    .get(function(req,res){
+router.route('/api/:name').get(function(req, res) {
+  var name = '$'+req.params.name;
+  Data.aggregate([{$project: {data: name}}], function(err, data){
+    res.jsonp(data[0].data);
+  })
+});
+router.route('/api/:name/:subname').get(function(req, res) {
+  var name = '$'+req.params.name + '.' + req.params.subname;
+  Data.aggregate([{$project: {data: name}}], function(err, data){
+    res.jsonp(data[0].data);
+  })
+});
+router.route('/api/:name/:subname/:type').get(function(req, res) {
+  var name = '$'+req.params.name+'.'+req.params.subname+'.'+req.params.type;
+  Data.aggregate([{$project: {data: name}}], function(err, data){
+    res.jsonp(data[0].data);
+  })
+});
+// router.route('/api/words/buzzwords').get(function(req, res) {
+//     Word.find(function(err, words) {
+//         if (err)
+//             res.send(err);
+//
+//         res.jsonp(words[0].toJSON().buzzwords);
+//     });
+// });
+  // router.route('/api/images/backgrounds').get(function(req,res){
+  //   Images.find(function(err, images) {
+  //     if (err)
+  //       res.send(err);
+  //
+  //     res.jsonp(JSON.parse('{"_id": "YtLT8trw4SBz", "images":'+JSON.stringify(images[0].toJSON().photos.backgrounds)+'}'));
+  //   });
+  // });
+  // router.route('/api/images/projects').get(function(req,res){
+  //     Images.find(function(err, images) {
+  //       console.log(images);
+  //       if (err)
+  //         res.send(err);
+  //
+  //       res.jsonp(images);
+  //     });
+  //   });
+    router.route('/api/images/projects/web').get(function(req,res){
       Images.find(function(err, images) {
+        if (err)
+          res.send(err);
+
+        res.jsonp(JSON.parse('{"_id": "QXefByhEm8v9", "images":'+JSON.stringify(images[0].toJSON().photos.projects.web)+'}'));
+      });
+    });
+    router.route('/api/images/projects/marketing').get(function(req,res){
+      Images.find(function(err, images) {
+        if (err)
+          res.send(err);
+
+        res.jsonp(JSON.parse('{"_id": "vMLdeLxMUDLr", "images":'+JSON.stringify(images[0].toJSON().photos.projects.marketing)+'}'));
+      });
+    });
+    router.route('/api/images/projects/brand').get(function(req,res){
+      Images.find(function(err, images) {
+        if (err)
+          res.send(err);
+
+        res.jsonp(JSON.parse('{"_id": "DzxEgXSdEQd2", "images":'+JSON.stringify(images[0].toJSON().photos.projects.brand)+'}'));
+      });
+    });
+    router.route('/api/images/projects/:type/:id').get(function(req,res){
+      Images.find({'photos.projects.*.id':req.params.id}, function(err, images) {
+        console.log(images);
         if (err)
           res.send(err);
 
         res.jsonp(images);
       });
     });
-    router.route('/api/bios').get(function(req,res){
-        Bio.find(function(err, bios) {
-          if (err)
-            res.send(err);
-
-          res.jsonp(bios);
-        });
-      });
-      router.route('/api/clients')
-        .get(function(req,res){
-          Client.find(function(err,clients) {
-            if (err)
-              res.send(err);
-
-            res.jsonp(clients);
-          });
-        });
-        router.route('/api/people')
-          .get(function(req,res){
-            Person.find(function(err,people) {
-              if (err)
-                res.send(err);
-
-              res.jsonp(people);
-            });
-          });
 // REGISTER OUR ROUTES -------------------------------
 app.use('/', router);
 
