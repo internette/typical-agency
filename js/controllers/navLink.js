15b4fe3e076c00c3b88fdf76fee8332ef2418f17
Antoinette Janus <acj.gfx@gmail.com>
2016-05-11 22:02:15 -0400

    Modified nav

--- js/controllers/navLink.js
+++ js/controllers/navLink.js
@@ -5,16 +5,21 @@ typicalAgency.controller('navLink', function($scope, $window, $document, $locati
   $scope.scrollDirection = 'down';
   $scope.clickEvent = function($event){
     $event.preventDefault();
-    if(!$location.$$absUrl.match(/team/gi) && !$event.target.id.match(/menu-btn/gi)){
-        $("html, body").animate({ scrollTop: $('#'+$event.target.href.split('#')[1]).offset().top-15 }, 750);
-    } else if ($event.target.id.match(/menu-btn/gi)){
+    console.log($event.target.parentNode.parentNode.parentNode.parentNode);
+    if ($event.target.id.match(/menu-btn/gi)){
       if($event.target.nextElementSibling.className.match(/active/gi)){
         $event.target.nextElementSibling.className = $event.target.nextElementSibling.className.replace(/ active/gi,'');
       } else {
         $event.target.nextElementSibling.className += ' active';
       }
-    } else {
+    }
+    if(!$location.$$absUrl.match(/team/gi) && !$event.target.id.match(/menu-btn/gi)){
+        $("html, body").animate({ scrollTop: $('#'+$event.target.href.split('#')[1]).offset().top-15 }, 750);
+    } else if ($event.target.parentNode.parentNode.parentNode.parentNode.id === 'nav' && !$event.target.id.match(/menu-btn/gi)){
       window.location = $location.$$absUrl.split(/\/team/gi)[0]
     }
+    // else {
+    //   window.location = $location.$$absUrl.split(/\/team/gi)[0]
+    // }
   }
 });
