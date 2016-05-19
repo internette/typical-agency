8e1a88d19dfb294b9166f26ca36d655b19d7ef57
Antoinette Janus <acj.gfx@gmail.com>
2016-05-19 16:54:45 -0400

    Force reset

--- js/controllers/favicon.js
+++ js/controllers/favicon.js
@@ -1,24 +1,8 @@
 typicalAgency.controller('Favicon', ['$scope', 'brandColor', '$window', 'agencyName', function($scope, brandColor, $window, agencyName){
   brandColor.then(function(data){
-    if($window.sessionStorage.getItem("brandColorValue")===null){
-      $window.sessionStorage.setItem("brandColorValue", data.value);
-      $scope.brandColorValue = $window.sessionStorage.getItem("brandColorValue");
-    } else {
-      $scope.brandColorValue = $window.sessionStorage.getItem("brandColorValue");
-    }
-    if($window.sessionStorage.getItem("brandColorName")===null){
-      $window.sessionStorage.setItem("brandColorName", data.value);
-      $scope.brandColorName = $window.sessionStorage.getItem("brandColorName");
-    } else {
-      $scope.brandColorName = $window.sessionStorage.getItem("brandColorName");
-    }
-  });
-  agencyName.then(function(data){
-    if($window.sessionStorage.getItem("agencyName")===null){
-      $window.sessionStorage.setItem("agencyName", data);
-      $scope.agencyName = $window.sessionStorage.getItem("agencyName");
-    } else {
-      $scope.agencyName = $window.sessionStorage.getItem("agencyName");
-    }
+    $window.sessionStorage.setItem("brandColorValue", data.value);
+    $window.sessionStorage.setItem("brandColorName", data.name);
+    $scope.brandColorValue = $window.sessionStorage.getItem("brandColorValue");
+    $scope.brandColorName = $window.sessionStorage.getItem("brandColorName");
   });
 }]);
